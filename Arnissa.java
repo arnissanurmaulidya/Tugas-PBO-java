c9ab56123bdbdab82895bdb10e2a3a26add7b9d5
arnissanurmaulidya <70264816+arnissanurmaulidya@users.noreply.github.com>
2020-08-26 19:10:36 +0700

    Add files via upload

---
+++ Arnissa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arnissa;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Arnissa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("nama siswa : arnissa nur maulidya");
+       System.out.println("jenis kelamin : perempuan");
+       System.out.println("alamat : lawang malang jawa timur");
+       System.out.println("jurusan : rekayasa perangkat lunak");
+    }
+    
+}
